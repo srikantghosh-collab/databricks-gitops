1f8e6accdfd9c04a70b7534e88a1b93269d9f4b0
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-15 16:59:43 +0530

    Enable column mapping

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,14 +1,6 @@
 ALTER TABLE employees
 SET TBLPROPERTIES ('delta.columnMapping.mode' = 'name');
 
-ALTER TABLE employees ADD COLUMN email STRING;
-
 ALTER TABLE employees RENAME COLUMN emp_name TO full_name;
 
-ALTER TABLE employees ALTER COLUMN salary COMMENT 'Monthly salary in INR';
-
-ALTER TABLE employees SET TBLPROPERTIES (
-  'quality' = 'silver',
-  'modified_by' = 'devops_pipeline'
-);
 
