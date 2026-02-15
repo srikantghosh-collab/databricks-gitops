3ffe6779d16ab500391c56146fdd6e22a54bef44
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-15 16:32:23 +0530

    ALTER TABLE employees_delta

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,3 +1,6 @@
+ALTER TABLE employees
+SET TBLPROPERTIES ('delta.columnMapping.mode' = 'name');
+
 ALTER TABLE employees ADD COLUMN email STRING;
 
 ALTER TABLE employees RENAME COLUMN emp_name TO full_name;
@@ -9,4 +12,3 @@ ALTER TABLE employees SET TBLPROPERTIES (
   'modified_by' = 'devops_pipeline'
 );
 
-ALTER TABLE employees RENAME TO employee_master;
