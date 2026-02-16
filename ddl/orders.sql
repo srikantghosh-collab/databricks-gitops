554b151a821a55866494448f7ee22661391717cd
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-17 02:21:09 +0530

    CREATE TABLE 18

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE employee_v17 (
+CREATE TABLE employee_v18 (
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -7,15 +7,15 @@ CREATE TABLE employee_v17 (
     created_date TIMESTAMP
  ) USING DELTA;
 
- INSERT INTO employee_v17 VALUES
+ INSERT INTO employee_v18 VALUES
  (1, 'Amit', 'IT', 70000, current_timestamp()),
  (2, 'Neha', 'HR', 55000, current_timestamp()),
  (3, 'Rohit', 'Finance', 80000, current_timestamp());
 
 
- ALTER TABLE employee_v17 ADD COLUMN email STRING;
+ ALTER TABLE employee_v18 ADD COLUMN email STRING;
 
- ALTER TABLE employee_v17 SET TBLPROPERTIES (
+ ALTER TABLE employee_v18 SET TBLPROPERTIES (
    'quality' = 'silver',
    'modified_by' = 'gitops_pipeline'
  );
--- scripts/detect_revert.py
+++ scripts/detect_revert.py
@@ -1,23 +1,34 @@
 import subprocess
 import sys
+import re
 
 try:
     msg = subprocess.check_output(
         ["git", "log", "-1", "--pretty=%B"],
         text=True
     )
-except:
+except Exception:
     print("##vso[task.setvariable variable=IS_REVERT;isOutput=true]false")
     sys.exit(0)
 
+# Check if this is a revert commit
 if msg.startswith("Revert"):
-    commit = subprocess.check_output(
-        ["git", "rev-parse", "HEAD~1"],
-        text=True
-    ).strip()
-
     print("Git revert detected")
-    print(f"##vso[task.setvariable variable=IS_REVERT;isOutput=true]true")
-    print(f"##vso[task.setvariable variable=REVERT_COMMIT;isOutput=true]{commit}")
+
+    # Extract original commit id from revert message
+    match = re.search(r"This reverts commit ([a-f0-9]+)", msg)
+
+    if not match:
+        print("ERROR: Could not extract reverted commit id")
+        print("##vso[task.setvariable variable=IS_REVERT;isOutput=true]false")
+        sys.exit(1)
+
+    reverted_commit = match.group(1)
+
+    print(f"Reverted commit detected: {reverted_commit}")
+
+    print("##vso[task.setvariable variable=IS_REVERT;isOutput=true]true")
+    print(f"##vso[task.setvariable variable=REVERT_COMMIT;isOutput=true]{reverted_commit}")
+
 else:
     print("##vso[task.setvariable variable=IS_REVERT;isOutput=true]false")
