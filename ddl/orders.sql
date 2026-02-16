4da1b143f89b3ac88e437472be0cfa99e7fab483
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-16 11:18:10 +0530

    DROP table

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,24 +1,24 @@
 
-'''CREATE TABLE employee_v5 (
-    emp_id INT,
-    emp_name STRING,
-    department STRING,
-    salary DECIMAL(10,2),
-    created_date TIMESTAMP
-) USING DELTA;
+-- '''CREATE TABLE employee_v5 (
+--     emp_id INT,
+--     emp_name STRING,
+--     department STRING,
+--     salary DECIMAL(10,2),
+--     created_date TIMESTAMP
+-- ) USING DELTA;
 
-INSERT INTO employee_v5 VALUES
-(1, 'Amit', 'IT', 70000, current_timestamp()),
-(2, 'Neha', 'HR', 55000, current_timestamp()),
-(3, 'Rohit', 'Finance', 80000, current_timestamp());
-
-
-ALTER TABLE employee_v5 ADD COLUMN email STRING;
+-- INSERT INTO employee_v5 VALUES
+-- (1, 'Amit', 'IT', 70000, current_timestamp()),
+-- (2, 'Neha', 'HR', 55000, current_timestamp()),
+-- (3, 'Rohit', 'Finance', 80000, current_timestamp());
 
 
-ALTER TABLE employee_v5 SET TBLPROPERTIES (
-  'quality' = 'silver',
-  'modified_by' = 'gitops_pipeline'
-);'''
+-- ALTER TABLE employee_v5 ADD COLUMN email STRING;
+
+
+-- ALTER TABLE employee_v5 SET TBLPROPERTIES (
+--   'quality' = 'silver',
+--   'modified_by' = 'gitops_pipeline'
+-- );'''
 
 DROP TABLE IF EXISTS employee_v5;
