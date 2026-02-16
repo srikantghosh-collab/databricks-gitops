c9e729afd9843922856d48d72c23fa1b8d01d9b7
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-17 02:47:53 +0530

    Revert "DROP TABLE 19"
    This reverts commit 2ea58679d00982da6482bd012eb701923d6f3d62.

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,23 +1,22 @@
 
--- CREATE TABLE employee_v19 (
---     emp_id INT,
---     emp_name STRING,
---     department STRING,
---     salary DECIMAL(10,2),
---     created_date TIMESTAMP
---  ) USING DELTA;
+CREATE TABLE employee_v19 (
+    emp_id INT,
+    emp_name STRING,
+    department STRING,
+    salary DECIMAL(10,2),
+    created_date TIMESTAMP
+ ) USING DELTA;
 
---  INSERT INTO employee_v19 VALUES
---  (1, 'Amit', 'IT', 70000, current_timestamp()),
---  (2, 'Neha', 'HR', 55000, current_timestamp()),
---  (3, 'Rohit', 'Finance', 80000, current_timestamp());
+ INSERT INTO employee_v19 VALUES
+ (1, 'Amit', 'IT', 70000, current_timestamp()),
+ (2, 'Neha', 'HR', 55000, current_timestamp()),
+ (3, 'Rohit', 'Finance', 80000, current_timestamp());
 
 
---  ALTER TABLE employee_v19 ADD COLUMN email STRING;
+ ALTER TABLE employee_v19 ADD COLUMN email STRING;
 
---  ALTER TABLE employee_v19 SET TBLPROPERTIES (
---    'quality' = 'silver',
---    'modified_by' = 'gitops_pipeline'
---  );
+ ALTER TABLE employee_v19 SET TBLPROPERTIES (
+   'quality' = 'silver',
+   'modified_by' = 'gitops_pipeline'
+ );
 
-DROP TABLE employee_v19;
