781225ca754710ffe96cf2825abac54f014638f7
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-16 10:52:56 +0530

    Table created

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE employee_v4 (
+CREATE TABLE employee_v5 (
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -7,16 +7,16 @@ CREATE TABLE employee_v4 (
     created_date TIMESTAMP
 ) USING DELTA;
 
-INSERT INTO employee_v4 VALUES
+INSERT INTO employee_v5 VALUES
 (1, 'Amit', 'IT', 70000, current_timestamp()),
 (2, 'Neha', 'HR', 55000, current_timestamp()),
 (3, 'Rohit', 'Finance', 80000, current_timestamp());
 
 
-ALTER TABLE employee_v4 ADD COLUMN email STRING;
+ALTER TABLE employee_v5 ADD COLUMN email STRING;
 
 
-ALTER TABLE employee_v4 SET TBLPROPERTIES (
+ALTER TABLE employee_v5 SET TBLPROPERTIES (
   'quality' = 'silver',
   'modified_by' = 'gitops_pipeline'
 );
