322822085b43be28e52fa281419e075836ae9900
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-18 09:36:53 +0530

    TRUNCATE TABLE

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS employee_v25 (
+CREATE TABLE IF NOT EXISTS employee_v26 (
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -7,16 +7,20 @@ CREATE TABLE IF NOT EXISTS employee_v25 (
     created_date TIMESTAMP
  ) USING DELTA;
 
-INSERT INTO employee_v25
+INSERT INTO employee_v26
 (emp_id, emp_name, department, salary, created_date)
 VALUES
 (1, 'Amit', 'IT', 70000, current_timestamp()),
 (2, 'Neha', 'HR', 55000, current_timestamp()),
 (3, 'Rohit', 'Finance', 80000, current_timestamp());
 
+ALTER TABLE employee_v26
+SET TBLPROPERTIES (
+  'delta.columnMapping.mode' = 'name'
+);
 
-ALTER TABLE employee_v25 DROP COLUMN department;
+ALTER TABLE employee_v26 DROP COLUMN department;
 
-ALTER TABLE employee_v25 ALTER COLUMN salary TYPE INT;
+ALTER TABLE employee_v26 ALTER COLUMN salary TYPE INT;
 
-TRUNCATE TABLE employee_v25;
+TRUNCATE TABLE employee_v26;
