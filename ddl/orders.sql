1265fff65f9233ee7def4f2e8b63a4fecbdcf7aa
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-18 09:27:47 +0530

    ALTER TABLE

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS employee_v24 (
+CREATE TABLE IF NOT EXISTS employee_v25 (
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -7,13 +7,16 @@ CREATE TABLE IF NOT EXISTS employee_v24 (
     created_date TIMESTAMP
  ) USING DELTA;
 
- INSERT INTO employee_v24 VALUES
- (1, 'Amit', 'IT', 70000, current_timestamp()),
- (2, 'Neha', 'HR', 55000, current_timestamp()),
- (3, 'Rohit', 'Finance', 80000, current_timestamp());
+INSERT INTO employee_v25
+(emp_id, emp_name, department, salary, created_date)
+VALUES
+(1, 'Amit', 'IT', 70000, current_timestamp()),
+(2, 'Neha', 'HR', 55000, current_timestamp()),
+(3, 'Rohit', 'Finance', 80000, current_timestamp());
 
-ALTER TABLE employee_v24 DROP COLUMN department;
 
-ALTER TABLE employee_v24 ALTER COLUMN salary TYPE INT;
+ALTER TABLE employee_v25 DROP COLUMN department;
 
-TRUNCATE TABLE employee_v24;
+ALTER TABLE employee_v25 ALTER COLUMN salary TYPE INT;
+
+TRUNCATE TABLE employee_v25;
