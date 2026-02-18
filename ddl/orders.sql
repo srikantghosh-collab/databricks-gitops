18aae90ade9104f81651e478756deaad7b7c98d7
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-18 18:34:07 +0530

    CREATE TABLE 29

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,39 +1,19 @@
-
-CREATE TABLE IF NOT EXISTS employee_v27 (
+CREATE TABLE IF NOT EXISTS employee_v29 (
     emp_id INT,
     emp_name STRING,
     department STRING,
     salary DECIMAL(10,2),
     created_date TIMESTAMP
- ) USING DELTA;
+)
+USING DELTA;
 
-INSERT INTO employee_v27
-(emp_id, emp_name, department, salary, created_date)
-VALUES
-(1, 'Amit', 'IT', 70000, current_timestamp()),
-(2, 'Neha', 'HR', 55000, current_timestamp()),
-(3, 'Rohit', 'Finance', 80000, current_timestamp());
+INSERT INTO employee_v29 VALUES
+    (1, 'John', 'IT', 60000, current_timestamp()),
+    (2, 'Sara', 'HR', 50000, current_timestamp()),
+    (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v27
-SET TBLPROPERTIES (
-  'delta.columnMapping.mode' = 'name'
+ALTER TABLE employee_v29 SET TBLPROPERTIES (
+    'delta.logRetentionDuration' = 'interval 30 days',
+    'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
-
-ALTER TABLE employee_v27 DROP COLUMN department;
-
-
-ALTER TABLE employee_v27 ADD COLUMN salary_int INT;
-
-
-UPDATE employee_v27 SET salary_int = CAST(salary AS INT);
-
-
-ALTER TABLE employee_v27 DROP COLUMN salary;
-
-
-ALTER TABLE employee_v27 RENAME COLUMN salary_int TO salary;
-
-
-
-
 
