1c127cbc8cba4696c49d6e80c597fa952c2fd06a
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-20 13:56:10 +0530

    CREATE TABLE

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -19,4 +19,5 @@ ALTER TABLE employee_v36 SET TBLPROPERTIES (
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
 
-ALTER TABLE employee_v36 ALTER COLUMN salary DECIMAL(14,2);
+ALTER TABLE employee_v36
+CHANGE COLUMN salary salary DECIMAL(14,2);
