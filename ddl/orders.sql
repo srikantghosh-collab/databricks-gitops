7900c6ff3aa758b812b2b38325132b8c860e8619
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-21 18:36:37 +0530

    DROP COLUMN

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,6 +1,5 @@
 
-
-CREATE TABLE IF NOT EXISTS employee_v39(
+CREATE TABLE IF NOT EXISTS employee_v40(
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -9,12 +8,12 @@ CREATE TABLE IF NOT EXISTS employee_v39(
 )
 USING DELTA;
 
-INSERT INTO employee_v39 VALUES
+INSERT INTO employee_v40 VALUES
     (1, 'John', 'IT', 60000, current_timestamp()),
     (2, 'Sara', 'HR', 50000, current_timestamp()),
     (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v39 SET TBLPROPERTIES (
+ALTER TABLE employee_v40 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
