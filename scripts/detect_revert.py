0ef50d4cb7c27cac5b05a89244f5118a79dcc203
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-25 00:32:39 +0530

    Revert "40"
    This reverts commit 05125f13ec3bf77af2ec583bc3f4100a754acbee.

--- scripts/detect_revert.py
+++ scripts/detect_revert.py
@@ -33,7 +33,7 @@ print("execution started")
 print(f"Detected git commit message: {msg}")
 #print(f"##vso[task.setvariable variable=IS_REVERT;isOutput=true]{str(is_revert).lower()}")
 print(f"##vso[task.setvariable variable=IS_REVERT;isOutput=true]{is_revert_str}")
-print(f"is_revert_str: {is_revert_str}")
+print(f"is_revert: {is_revert}")
 print(f"##vso[task.setvariable variable=REVERT_COMMIT;isOutput=true]{revert_commit}")
 print(f"revert_commit: {revert_commit}")
 # exit 0 so pipeline continues
