c7aac56312cab3cf430ea9e67409dd5edb1b2c7d
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-24 19:51:18 +0530

    Revert "interval 40 days"
    This reverts commit 34384971aa153fa4574692c917f331445f25ebec.

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -14,8 +14,8 @@ INSERT INTO employee_v62  VALUES
     (3, 'Mike', 'Finance', 70000, current_timestamp());
 
 ALTER TABLE employee_v62 SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 40 days',
-    'delta.deletedFileRetentionDuration' = 'interval 40 days'
+    'delta.logRetentionDuration' = 'interval 30 days',
+    'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
 
  
