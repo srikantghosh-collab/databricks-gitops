be1e65c9a780a14dbb843c3f32e4949e62403492
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-25 11:54:48 +0530

    CREATE TABLE 67

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS employee_v66(
+CREATE TABLE IF NOT EXISTS employee_v67(
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS employee_v66(
 )
 USING DELTA;
 
-INSERT INTO employee_v66  VALUES
+INSERT INTO employee_v67  VALUES
     (1, 'John', 'IT', 60000, current_timestamp()),
     (2, 'Sara', 'HR', 50000, current_timestamp()),
     (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v66 SET TBLPROPERTIES (
+ALTER TABLE employee_v67 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
