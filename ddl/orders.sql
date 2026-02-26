f441b16a5eeb666ac865b868476b861b93e777c6
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-27 02:20:37 +0530

    CREATE TABLE 83

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS employee_v82(
+CREATE TABLE IF NOT EXISTS employee_v83(
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS employee_v82(
 )
 USING DELTA;
 
-INSERT INTO employee_v82  VALUES
+INSERT INTO employee_v83  VALUES
     (1, 'John', 'IT', 60000, current_timestamp()),
     (2, 'Sara', 'HR', 50000, current_timestamp()),
     (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v82 SET TBLPROPERTIES (
+ALTER TABLE employee_v83 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );
--- scripts/detect_revert.py
+++ scripts/detect_revert.py
@@ -32,8 +32,6 @@ revert_commit = reverted or ""
 print("execution started")
 print(f"Detected git commit message: {msg}")
 #print(f"##vso[task.setvariable variable=IS_REVERT;isOutput=true]{str(is_revert).lower()}")
-print(f"##vso[task.setvariable variable=PIPELINE_IS_REVERT]{is_revert_str}")
-print(f"##vso[task.setvariable variable=PIPELINE_REVERT_COMMIT]{revert_commit}")
 print(f"##vso[task.setvariable variable=is_revert_str;isOutput=true]{is_revert_str}")
 print(f"is_revert: {is_revert}")
 print(f"##vso[task.setvariable variable=REVERT_COMMIT;isOutput=true]{revert_commit}")
