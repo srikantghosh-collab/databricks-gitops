2cbaabed6da51a3b17e0ecb8e919e4262d4e3f4d
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-26 14:46:25 +0530

    DROP 77

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,21 +1,22 @@
 
-CREATE TABLE IF NOT EXISTS employee_v78(
-    emp_id INT,
-    emp_name STRING,
-    department STRING,
-    salary DECIMAL(10,2),
-    created_date TIMESTAMP
-)
-USING DELTA;
+-- CREATE TABLE IF NOT EXISTS employee_v78(
+--     emp_id INT,
+--     emp_name STRING,
+--     department STRING,
+--     salary DECIMAL(10,2),
+--     created_date TIMESTAMP
+-- )
+-- USING DELTA;
 
-INSERT INTO employee_v78  VALUES
-    (1, 'John', 'IT', 60000, current_timestamp()),
-    (2, 'Sara', 'HR', 50000, current_timestamp()),
-    (3, 'Mike', 'Finance', 70000, current_timestamp());
+-- INSERT INTO employee_v78  VALUES
+--     (1, 'John', 'IT', 60000, current_timestamp()),
+--     (2, 'Sara', 'HR', 50000, current_timestamp()),
+--     (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v78 SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 30 days',
-    'delta.deletedFileRetentionDuration' = 'interval 30 days'
-);
+-- ALTER TABLE employee_v78 SET TBLPROPERTIES (
+--     'delta.logRetentionDuration' = 'interval 30 days',
+--     'delta.deletedFileRetentionDuration' = 'interval 30 days'
+-- );
 
 
+DROP TABLE employee_v77;
