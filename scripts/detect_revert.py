c3bfcea9c377d76f56a5fe1ad6b9074c6c6b8d75
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-02-27 13:05:44 +0530

    CREATE TABLE 83

--- scripts/detect_revert.py
+++ scripts/detect_revert.py
@@ -34,7 +34,7 @@ print(f"Detected git commit message: {msg}")
 #print(f"##vso[task.setvariable variable=IS_REVERT;isOutput=true]{str(is_revert).lower()}")
 
 
-print(f"##vso[task.setvariable variable=REVERT_COMMIT]{revert_commit}")
+print(f"##vso[task.setvariable variable=REVERT_COMMIT;isOutput=true]{revert_commit}")
 
-print(f"##vso[task.setvariable variable=PIPELINE_IS_REVERT]{is_revert_str}")
+print(f"##vso[task.setvariable variable=PIPELINE_IS_REVERT;isOutput=true]{is_revert_str}")
 # exit 0 so pipeline continues
