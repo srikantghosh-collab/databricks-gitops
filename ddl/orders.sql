846b3dd6895308bddf7140a1b731d986f5e39f35
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-04 23:08:55 +0530

    CREATE SCEHMA

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,22 +1,23 @@
-CREATE TABLE IF NOT EXISTS employee_v97 (
-    emp_id INT,
-    emp_name STRING,
-    department STRING,
-    salary DECIMAL(10,2),
-    created_date TIMESTAMP
-)
-USING DELTA;
+-- CREATE TABLE IF NOT EXISTS employee_v97 (
+--     emp_id INT,
+--     emp_name STRING,
+--     department STRING,
+--     salary DECIMAL(10,2),
+--     created_date TIMESTAMP
+-- )
+-- USING DELTA;
+-- PARTITIONED BY (department);
+-- OPTIMIZE employee_v97
+-- ZORDER BY ();
 
-INSERT INTO employee_v97 VALUES
-(1, 'John', 'IT', 60000, current_timestamp()),
-(2, 'Sara', 'HR', 50000, current_timestamp()),
-(3, 'Mike', 'Finance', 70000, current_timestamp());
+-- INSERT INTO employee_v97 VALUES
+-- (1, 'John', 'IT', 60000, current_timestamp()),
+-- (2, 'Sara', 'HR', 50000, current_timestamp()),
+-- (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_v97
-SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 30 days',
-    'delta.deletedFileRetentionDuration' = 'interval 30 days'
-);
-PARTITIONED BY (department);
-OPTIMIZE employee_v97
-ZORDER BY (department);
+-- ALTER TABLE employee_v97
+-- SET TBLPROPERTIES (
+--     'delta.logRetentionDuration' = 'interval 30 days',
+--     'delta.deletedFileRetentionDuration' = 'interval 30 days'
+-- );
+CREATE SCHEMA demo_ddl_db;
