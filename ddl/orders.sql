2a67987a8ef28c5e5cf833868363419d4da539ea
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-04 12:35:55 +0530

    ZORDER

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,12 +1,12 @@
--- CREATE TABLE orders_v1
--- (
--- order_id INT,
--- customer_id INT,
--- amount DOUBLE,
--- order_date DATE
--- )
--- USING DELTA
--- PARTITIONED BY (order_date);
+CREATE TABLE orders_v1 (
+order_id INT,
+customer_id INT,
+amount DOUBLE,
+order_date DATE
+)
+USING DELTA
+PARTITIONED BY (order_date);
 
+-- Optimization
 OPTIMIZE orders_v1
-ZORDER BY (customer_id)
+ZORDER BY (customer_id);
