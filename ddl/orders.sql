530028c2151b87bc683c58fd7637fd755fe11cce
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-09 18:39:38 +0530

    ALTER pro11

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,21 +1,33 @@
-CREATE TABLE IF NOT EXISTS employee_pro11 (
-    emp_id INT,
-    emp_name STRING,
-    department STRING,
-    salary DECIMAL(10,2),
-    created_date TIMESTAMP
-)
-USING DELTA;
+-- CREATE TABLE IF NOT EXISTS employee_pro11 (
+--     emp_id INT,
+--     emp_name STRING,
+--     department STRING,
+--     salary DECIMAL(10,2),
+--     created_date TIMESTAMP
+-- )
+-- USING DELTA;
 
-INSERT INTO employee_pro11 VALUES
-(1, 'John', 'IT', 60000, current_timestamp()),
-(2, 'Sara', 'HR', 50000, current_timestamp()),
-(3, 'Mike', 'Finance', 70000, current_timestamp());
+-- INSERT INTO employee_pro11 VALUES
+-- (1, 'John', 'IT', 60000, current_timestamp()),
+-- (2, 'Sara', 'HR', 50000, current_timestamp()),
+-- (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_pro11 SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 30 days',
-    'delta.deletedFileRetentionDuration' = 'interval 30 days'
-);
+-- ALTER TABLE employee_pro11 SET TBLPROPERTIES (
+--     'delta.logRetentionDuration' = 'interval 30 days',
+--     'delta.deletedFileRetentionDuration' = 'interval 30 days'
+-- );
+ALTER TABLE employee_pro11 ADD COLUMNS (email STRING);
+
+ALTER TABLE employee_pro11 RENAME COLUMN emp_name TO full_name;
+
+ALTER TABLE employee_pro11 ALTER COLUMN salary COMMENT 'Monthly salary in INR';
+
+ALTER TABLE employee_pro11 SET TBLPROPERTIES ( 'quality' = 'silver',
+'modified_by' = 'devops_pipeline' );
+
+ALTER TABLE employee_pro11 RENAME TO employee_sigmoid;
+
+
 
 
 -- CREATE TABLE orders (
@@ -28,14 +40,3 @@ ALTER TABLE employee_pro11 SET TBLPROPERTIES (
 
 -- OPTIMIZE orders
 -- ZORDER BY (customer_id);
-
--- ALTER TABLE employee_pro9 ADD COLUMNS (email STRING);
-
--- ALTER TABLE employee_pro9 RENAME COLUMN emp_name TO full_name;
-
--- ALTER TABLE employee_pro9 ALTER COLUMN salary COMMENT 'Monthly salary in INR';
-
--- ALTER TABLE employee_pro9 SET TBLPROPERTIES ( 'quality' = 'silver',
--- 'modified_by' = 'devops_pipeline' );
-
--- ALTER TABLE employee_pro9 RENAME TO employee_professional;
