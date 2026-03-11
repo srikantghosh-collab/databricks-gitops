da96a94fc0908cbef44b6402a0882a84e79395f6
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-11 07:06:04 +0530

    ALTER pro13

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,29 +1,29 @@
 
-CREATE TABLE IF NOT EXISTS employee_pro13 (
-    emp_id INT,
-    emp_name STRING,
-    department STRING,
-    salary DECIMAL(10,2),
-    created_date TIMESTAMP
-)
-USING DELTA;
+-- CREATE TABLE IF NOT EXISTS employee_pro13 (
+--     emp_id INT,
+--     emp_name STRING,
+--     department STRING,
+--     salary DECIMAL(10,2),
+--     created_date TIMESTAMP
+-- )
+-- USING DELTA;
 
-INSERT INTO employee_pro13 VALUES
-(1, 'John', 'IT', 60000, current_timestamp()),
-(2, 'Sara', 'HR', 50000, current_timestamp()),
-(3, 'Mike', 'Finance', 70000, current_timestamp());
+-- INSERT INTO employee_pro13 VALUES
+-- (1, 'John', 'IT', 60000, current_timestamp()),
+-- (2, 'Sara', 'HR', 50000, current_timestamp()),
+-- (3, 'Mike', 'Finance', 70000, current_timestamp());
 
-ALTER TABLE employee_pro13 SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 30 days',
-    'delta.deletedFileRetentionDuration' = 'interval 30 days'
-);
--- ALTER TABLE employee_pro1 ADD COLUMNS (email STRING);
+-- ALTER TABLE employee_pro13 SET TBLPROPERTIES (
+--     'delta.logRetentionDuration' = 'interval 30 days',
+--     'delta.deletedFileRetentionDuration' = 'interval 30 days'
+-- );
+ALTER TABLE employee_pro13 ADD COLUMNS (email STRING);
 
--- ALTER TABLE employee_pro1 RENAME COLUMN emp_name TO full_name;
+ALTER TABLE employee_pro13 RENAME COLUMN emp_name TO full_name;
 
--- ALTER TABLE employee_pro1 DROP COLUMN department;
+ALTER TABLE employee_pro13 DROP COLUMN department;
 
--- ALTER TABLE employee_pro1 ALTER COLUMN salary TYPE INT;
+ALTER TABLE employee_pro13 ALTER COLUMN salary TYPE INT;
 
 -- ALTER TABLE employee_pro12 ALTER COLUMN salary COMMENT 'Monthly salary in INR';
 
