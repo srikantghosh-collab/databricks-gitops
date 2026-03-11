ff20e41c420518d86739ca5bf49aef4318854c65
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-11 17:15:54 +0530

    pro14

--- ddl/orders.sql
+++ ddl/orders.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS employee_pro7 (
+CREATE TABLE IF NOT EXISTS employee_pro14 (
     emp_id INT,
     emp_name STRING,
     department STRING,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS employee_pro7 (
 )
 USING DELTA;
 
-INSERT INTO employee_pro7 VALUES
+INSERT INTO employee_pro14 VALUES
 (1, 'John', 'IT', 60000, current_timestamp()),
 (2, 'Sara', 'HR', 50000, current_timestamp()),
 (3, 'Mike', 'Finance', 70000, current_timestamp()),
@@ -20,19 +20,19 @@ INSERT INTO employee_pro7 VALUES
 (9, 'Ava', 'IT', 64000, current_timestamp()),
 (10, 'William', 'Finance', 75000, current_timestamp());
 
-ALTER TABLE employee_pro7 SET TBLPROPERTIES (
+ALTER TABLE employee_pro14 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 );   
--- ALTER TABLE employee_pro7 ADD COLUMNS (email STRING);
+-- ALTER TABLE employee_pro14 ADD COLUMNS (email STRING);
 
--- ALTER TABLE employee_pro7 RENAME COLUMN emp_name TO full_name;
+-- ALTER TABLE employee_pro14 RENAME COLUMN emp_name TO full_name;
 
--- ALTER TABLE employee_pro7 DROP COLUMN department;
+-- ALTER TABLE employee_pro14 DROP COLUMN department;
 
--- ALTER TABLE employee_pro7 ALTER COLUMN salary TYPE INT;
+-- ALTER TABLE employee_pro14 ALTER COLUMN salary TYPE INT;
 
--- ALTER TABLE employee_pro7 ALTER COLUMN salary COMMENT 'Monthly salary in INR';
+-- ALTER TABLE employee_pro14 ALTER COLUMN salary COMMENT 'Monthly salary in INR';
 
 -- ALTER TABLE employee_pro7 SET TBLPROPERTIES ( 'quality' = 'silver',
 -- 'modified_by' = 'devops_pipeline' );
