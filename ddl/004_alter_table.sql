822fa467c2fe6fdeaf53b0abd80cbe6103505a28
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-15 21:41:08 +0530

    altering the table

---
+++ ddl/004_alter_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE employee_pro14 ADD COLUMNS (email STRING);
+
+ALTER TABLE employee_pro14 RENAME COLUMN emp_name TO full_name;
+
+ALTER TABLE employee_pro14 DROP COLUMN department;
+
+ALTER TABLE employee_pro14 ALTER COLUMN salary TYPE INT;
