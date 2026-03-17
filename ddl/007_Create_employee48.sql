71e8f4745f78b2fa892f84dd2b4e554396b48d30
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-17 20:07:10 +0530

    C48

--- ddl/007_Create_employee48.sql
+++ ddl/007_Create_employee48.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-CREATE TABLE IF NOT EXISTS employee_pro47 (
+CREATE TABLE IF NOT EXISTS employee_pro48 (
     emp_id INT,
     emp_name STRING,
     department STRING,
--- ddl/009_set_table_properties48.sql
+++ ddl/009_set_table_properties48.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-ALTER TABLE employee_pro47 SET TBLPROPERTIES (
+ALTER TABLE employee_pro48 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 ); 
