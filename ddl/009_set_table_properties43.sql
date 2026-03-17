d72060c42e5d412ddf0ee771f5f8b6c2fd191331
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-17 17:50:19 +0530

    C&A43

--- ddl/009_set_table_properties43.sql
+++ ddl/009_set_table_properties43.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-ALTER TABLE employee_pro42 SET TBLPROPERTIES (
+ALTER TABLE employee_pro43 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 ); 
