4c2a36a8a40ff28ebb58fb8f425fdfc44f225251
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-17 12:13:50 +0530

    Revert "T30"
    This reverts commit 50c7e4bdeaae96f80586bceff88287101e2101de.

--- ddl/007_tbl_properties.sql
+++
@@ -1,7 +0,0 @@
-USE CATALOG hive_metastore;
-USE SCHEMA sigmoid_employee;
-
-ALTER TABLE employee_pro30 SET TBLPROPERTIES (
-    'delta.logRetentionDuration' = 'interval 30 days',
-    'delta.deletedFileRetentionDuration' = 'interval 30 days'
-); 
