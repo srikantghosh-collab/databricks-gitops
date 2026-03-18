5a03bdc4542ce83c5a85d97a468e5544b9e7eabe
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-18 18:46:11 +0530

    Revert "C&A71"
    This reverts commit 248aedd7b19d8ff739f15a4be284e39c53d2c86d.

--- ddl/007_Create_employee70.sql
+++ ddl/007_Create_employee70.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA demo_ddl_db;
 
-CREATE TABLE IF NOT EXISTS employee_pro71 (
+CREATE TABLE IF NOT EXISTS employee_pro69 (
     emp_id INT,
     emp_name STRING,
     department STRING,
