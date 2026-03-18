d6816d83dc22fe1b7372a993ca41f10aef67223b
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-18 14:58:04 +0530

    C&A62

--- ddl/009_set_table_properties62.sql
+++ ddl/009_set_table_properties62.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA demo_ddl_db;
 
-ALTER TABLE employee_pro60 SET TBLPROPERTIES (
+ALTER TABLE employee_pro62 SET TBLPROPERTIES (
     'delta.logRetentionDuration' = 'interval 30 days',
     'delta.deletedFileRetentionDuration' = 'interval 30 days'
 ); 
---
+++ ddl/010_ALTER62_table.sql
@@ -0,0 +1,10 @@
+USE CATALOG hive_metastore;
+USE SCHEMA sigmoid_employee;
+
+ALTER TABLE employee_pro48 ADD COLUMN email STRING;
+
+ALTER TABLE employee_pro48 RENAME COLUMN emp_name TO full_name;
+
+ALTER TABLE employee_pro48 DROP COLUMN department;
+
+ALTER TABLE employee_pro48 ALTER COLUMN salary TYPE INT;
