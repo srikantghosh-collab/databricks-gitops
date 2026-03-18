a88d5df9a249328006206c0c8ebae727b52deccc
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-18 16:16:49 +0530

    Revert "C&A63"
    This reverts commit 8bd24fe3454aca8f45717d88e45e1f0a9b3ee61a.

--- ddl/010_ALTER63_table.sql
+++
@@ -1,9 +0,0 @@
-USE CATALOG hive_metastore;
-USE SCHEMA demo_ddl_db;
-
-ALTER TABLE employee_pro63 ADD COLUMN email STRING;
-
-ALTER TABLE employee_pro63 RENAME COLUMN emp_name TO full_name;
-
-ALTER TABLE employee_pro63 DROP COLUMN department;
-
