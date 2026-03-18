691ad29ddba3cc00f0bc374d918c3a0eb0db60e4
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-18 16:38:10 +0530

    C&A64

--- ddl/010_ALTER62_table.sql
+++ ddl/010_ALTER62_table.sql
@@ -1,10 +1,11 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-ALTER TABLE employee_pro48 ADD COLUMN email STRING;
+ALTER TABLE employee_pro64 ADD COLUMN email STRING;
 
-ALTER TABLE employee_pro48 RENAME COLUMN emp_name TO full_name;
+ALTER TABLE employee_pro64 RENAME COLUMN emp_name TO full_name;
 
-ALTER TABLE employee_pro48 DROP COLUMN department;
+ALTER TABLE employee_pro64 ALTER COLUMN salary TYPE INT;
 
-ALTER TABLE employee_pro48 ALTER COLUMN salary TYPE INT;
+ALTER TABLE employee_pro64 DROP COLUMN department;
+
