66d9b2bde32734ca95f43452e14e708ebefbe78a
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-19 18:57:47 +0530

    85

--- ddl/008_Create_employee85.sql
+++ ddl/008_Create_employee85.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-CREATE TABLE IF NOT EXISTS employee_pro84 (
+CREATE TABLE IF NOT EXISTS employee_pro85 (
     emp_id INT,
     emp_name STRING,
     department STRING,
--- ddl/010_ALTER85.sql
+++ ddl/010_ALTER85.sql
@@ -1,10 +1,10 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-ALTER TABLE employee_pro29 ADD COLUMNS (email STRING);
+ALTER TABLE employee_pro85 ADD COLUMNS (email STRING);
 
-ALTER TABLE employee_pro29 RENAME COLUMN emp_name TO full_name;
+ALTER TABLE employee_pro85 RENAME COLUMN emp_name TO full_name;
 
-ALTER TABLE employee_pro29 DROP COLUMN department;
+ALTER TABLE employee_pro85 DROP COLUMN department;
 
-ALTER TABLE employee_pro29 ALTER COLUMN salary TYPE INT;
+ALTER TABLE employee_pro85 ALTER COLUMN salary TYPE INT;
