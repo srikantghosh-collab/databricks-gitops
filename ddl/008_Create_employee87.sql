b307624744acbac6efab2c59a95402f0bd913d2a
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-19 21:25:50 +0530

    Revert "C&A88"
    This reverts commit 819dc18567a46780d960593762d18d08847d78da.

--- ddl/008_Create_employee87.sql
+++ ddl/008_Create_employee87.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-CREATE TABLE IF NOT EXISTS employee_pro88 (
+CREATE TABLE IF NOT EXISTS employee_pro87 (
     emp_id INT,
     emp_name STRING,
     department STRING,
