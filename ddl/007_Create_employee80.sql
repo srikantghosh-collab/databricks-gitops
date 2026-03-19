24c45ce916d4bc6236d4f72f5ca00950b1aa28d0
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-19 13:58:21 +0530

    C&A80

--- ddl/007_Create_employee80.sql
+++ ddl/007_Create_employee80.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA demo_ddl_db;
 
-CREATE TABLE IF NOT EXISTS employee_pro80- (
+CREATE TABLE IF NOT EXISTS employee_pro80 (
     emp_id INT,
     emp_name STRING,
     department STRING,
