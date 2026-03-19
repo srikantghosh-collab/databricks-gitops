6af0eb7e85b7e86ee29f60deb97341a4d9d5596e
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-19 18:57:37 +0530

    85

---
+++ ddl/008_Create_employee85.sql
@@ -0,0 +1,11 @@
+USE CATALOG hive_metastore;
+USE SCHEMA sigmoid_employee;
+
+CREATE TABLE IF NOT EXISTS employee_pro84 (
+    emp_id INT,
+    emp_name STRING,
+    department STRING,
+    salary DECIMAL(10,2),
+    created_date TIMESTAMP
+)
+USING DELTA;
