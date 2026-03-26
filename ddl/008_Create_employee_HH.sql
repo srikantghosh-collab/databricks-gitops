aae80545c91f05c8bc858fbb32dca2e5dac5b230
srikantghosh-collab <srikant.ghosh@sigmoidanalytics.com>
2026-03-26 10:47:36 +0530

    HH

--- ddl/008_Create_employee_HH.sql
+++ ddl/008_Create_employee_HH.sql
@@ -1,7 +1,7 @@
 USE CATALOG hive_metastore;
 USE SCHEMA sigmoid_employee;
 
-CREATE TABLE IF NOT EXISTS employee_GG(
+CREATE TABLE IF NOT EXISTS employee_HH(
     emp_id INT,
     emp_name STRING,
     department STRING,
